6cfb79ec1ec76346745efbbbf9146131e15d5c63
shkurkoka <shkurkoka@gmail.com>
2023-05-20 11:42:40 +0300

    fix

--- js/task-02.js
+++ js/task-02.js
@@ -9,9 +9,20 @@ const ingredients = [
 
 const ingredientsList = document.querySelector('#ingredients');
 
-for (let i = 0; i < ingredients.length; i++) {
-  const ingredient = document.createElement("li");
-  ingredient.classList.add("item");
-  ingredient.textContent = `${ingredients[i]}`;
-  ingredientsList.append(ingredient);
-}
+const ingredientsListItems = ingredients
+  .map((ingredient) => {return `
+    <li class="item">
+      ${ingredient}
+    </li>
+  `})
+  .join("");
+
+ingredientsList.insertAdjacentHTML("beforeend", ingredientsListItems);
+
+
+// for (let i = 0; i < ingredients.length; i++) {
+//   const ingredient = document.createElement("li");
+//   ingredient.classList.add("item");
+//   ingredient.textContent = `${ingredients[i]}`;
+//   ingredientsList.append(ingredient);
+// };
